c053e50ff78cd41bcca0a31adb4b8c201b0825cd
Gnanesh-2007 <reddygnanesh20@gmail.com>
2026-09-23 13:02:28 +0530

    fix: add missing flutter/services.dart import for HapticFeedback in profile_screen.dart

--- app/lib/screens/profile_screen.dart
+++ app/lib/screens/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
